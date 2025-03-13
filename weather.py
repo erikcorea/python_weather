4f93aaa31891c0ddc494bb37b34397864afb49b2
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2025-03-13 10:38:21 -0400

    buildin url

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 from configparser import ConfigParser
 import argparse
+from urllib import parse
 
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def read_user_cli_args():
   parser = argparse.ArgumentParser(
@@ -17,6 +19,27 @@ def read_user_cli_args():
   )
   return parser.parse_args()
 
+def build_weather_query(city_input, metric=False):
+  """
+  Builds the URL for the API request to OpenWeather
+    
+  Args:
+    city_input (list[str]): Name of a city as collected by argparse
+    metric (bool): Whether or not to use metric units
+  
+  Returns:
+    str: URL formatted for a call to OpenWeather's city name endpoint
+  """
+  api_key = _get_api_key()
+  city_name = " ".join(city_input)
+  url_encoded_city_name = parse.quote_plus(city_name)
+  units = "metric" if metric else "imperial"
+  url = (
+    f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+    f"&units={units}&appid={api_key}"
+  )
+  return url
+
 def _get_api_key():
   """
   Fetch the API key from your config file.
@@ -31,4 +54,5 @@ def _get_api_key():
 
 if __name__ == "__main__":
   user_args = read_user_cli_args()
-  print(user_args, user_args.metric)
+  query_url = build_weather_query(user_args.city, user_args.metric)
+  print(query_url)
